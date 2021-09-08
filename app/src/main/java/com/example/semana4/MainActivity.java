73dc0ae37fe5f15eb39b264b319ca993af9f257e
AHTepsilon <alehertoro725@gmail.com>
2021-09-08 16:19:01 -0500

    Added layouts and ip showcase in first screen

---
+++ app/src/main/java/com/example/semana4/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.semana4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button searchButton;
+    Socket socket;
+
+    BufferedReader reader;
+    BufferedWriter writer;
+
+    TextView ipShowcaser;
+    InetAddress ipCommon;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        searchButton = findViewById(R.id.searchBtn);
+        ipShowcaser = findViewById(R.id.ipShow);
+
+        initClient();
+    }
+
+    public void initClient()
+    {
+        new Thread(
+                () ->
+                {
+                    try {
+                        ipCommon = InetAddress.getLocalHost();
+                        ipShowcaser.setText(ipCommon.toString());
+                        socket = new Socket("192.168.1.9", 4000);
+
+                        InputStream is = socket.getInputStream();
+                        InputStreamReader isr = new InputStreamReader(is);
+                        reader = new BufferedReader(isr);
+
+                        OutputStream os = socket.getOutputStream();
+                        OutputStreamWriter osr = new OutputStreamWriter(os);
+                        writer = new BufferedWriter(osr);
+
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+        ).start();
+    }
+}
